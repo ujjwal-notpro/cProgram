b683955a708fbdf3796c62507130a21b2ca9b04f
Ujjwal gupta <ujjwa@Ujjwal060406.com>
2025-10-01 01:02:21 +0530

    4  table

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+//  print 4 times table
 int main() {
     for (int i = 1; i <= 10; i++) {
-        printf("3 x %d = %d\n", i, 3 * i);
+        printf("4 x %d = %d\n", i, 4 * i);
     }
     return 0;
 }
